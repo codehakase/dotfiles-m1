dd6cc8f4a5189c60659dfdcbb16c8b341371ae4d
Francis Sunday <codehakase@gmail.com>
2022-05-12 11:22:26 +0100

    Auto Syncing dotfiles.... 22-05-12

---
+++ init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Download vim plug
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+	curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Sync nvimrc file
+if [ ! -d $HOME/.config/nvim ]; then
+	mkdir -p $HOME/.config/nvim
+	ln -s $PWD/neovim/init.vim $HOME/.config/nvim/init.vim
+else
+	if [ ! -e $HOME/.config/nvim/init.vim ]; then
+		rm $HOME/.config/nvim/init.vim
+	fi
+	ln -s $PWD/neovim/init.vim $HOME/.config/nvim/init.vim
+fi
+
+# Sync Coc config
+if [ ! -e $HOME/.config/nvim/coc-settings.json ]; then
+  rm $HOME/.config/nvim/coc-settings.json
+fi
+ln -s $PWD/config/coc-settings.json $HOME/.config/nvim/coc-settings.json
+
+# Sync tmux config
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+	git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm" --quiet
+	if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
+		tmux source-file "$HOME/.tmux.conf"
+	fi
+fi
+
+# Sync and load tmux config
+if [ ! -e $HOME/.tmux.conf ]; then
+  ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
+fi
---
+++ sync.ex
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "synching dotfiles to vcs..."
+
+now=`date '+%y-%m-%d'`
+
+cd $HOME/dotfiles
+
+git add .
+git commit -m "Auto Syncing dotfiles.... $now"
+git push
